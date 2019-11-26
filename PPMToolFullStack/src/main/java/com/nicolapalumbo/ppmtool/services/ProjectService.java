4eebc0472c4086aa90142814d66e4422d2d7ff98
palnic <nicola.palumbo1993@gmail.com>
2019-11-26 12:00:58 +0100

    Implemented findByIdentifier and findAll methods

--- PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/services/ProjectService.java
@@ -12,15 +12,32 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
+    public Project saveOrUpdateProject(Project project) {
 
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
-        }catch (Exception e){
-            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase());
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
 
+
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID:" + projectId + " does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+
+        return projectRepository.findAll();
 
     }
 
