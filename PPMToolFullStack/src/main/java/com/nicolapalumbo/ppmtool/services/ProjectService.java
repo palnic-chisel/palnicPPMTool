d03ff6c1325f54800511d9aa5e01e214016b384b
palnic <nicola.palumbo1993@gmail.com>
2019-11-26 17:45:42 +0100

    Created FE React App

--- PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/services/ProjectService.java
@@ -41,4 +41,16 @@ public class ProjectService {
 
     }
 
+    public void deleteProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete Project with ID " + projectId + ". This project does not exist");
+        }
+
+        projectRepository.delete(project);
+
+    }
+
 }
--- PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/nicolapalumbo/ppmtool/web/ProjectController.java
@@ -4,18 +4,14 @@ package com.nicolapalumbo.ppmtool.web;
 import com.nicolapalumbo.ppmtool.services.MapValidationErrorService;
 import com.nicolapalumbo.ppmtool.services.ProjectService;
 import com.nicolapalumbo.ppmtool.domain.Project;
-import com.nicolapalumbo.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -51,6 +47,15 @@ public class ProjectController {
     public Iterable<Project> getAllProjects(){
 
         return projectService.findAllProjects();
+
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProjectById(@PathVariable String projectId){
+
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: " + projectId + " was deleted", HttpStatus.OK);
 
     }
 
